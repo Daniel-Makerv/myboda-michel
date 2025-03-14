28225110e4c2decb80eacbc08a307de619a71e92
Jose daniel De la cruz rivera <josedanieldelacruzrivera@MacBook-Air-de-Jose.local>
2025-03-14 14:00:00 -0600

    imagenes del salon y misa

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import Invite from './components/Invite.vue'
+import { Analytics } from '@vercel/analytics/vue';
+// import Index from './components/Index.vue'
+
 </script>
 
 <template>
+  <Analytics />
   <Invite />
 </template>
