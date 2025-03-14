2a9ed5774ed309d35641d3ebbd0deee95136a859
Jose daniel De la cruz rivera <josedanieldelacruzrivera@MacBook-Air-de-Jose.local>
2025-03-14 14:02:29 -0600

    imagenes del salon y misa

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import Invite from './components/Invite.vue'
-import { Analytics } from '@vercel/analytics/vue';
+// import { Analytics } from '@vercel/analytics/vue';
 // import Index from './components/Index.vue'
 
 </script>
 
 <template>
-  <Analytics />
+  <!-- <Analytics /> -->
   <Invite />
 </template>
